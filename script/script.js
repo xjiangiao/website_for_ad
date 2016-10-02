6300082d68b834f5767fb56970b426d54d66806c
jxiao5@stevens.edu <xjtc19910103@gmail.com>
2016-10-02 18:37:18 -0400

    redesign the advanatege area

--- script/script.js
+++ script/script.js
@@ -1,61 +1,23 @@
-var login_form =  $('#login_form');
-var register_form = $('#register_form');
-var forget_form = $('#forget_form');
-var modal_form = $('#modal_form');
-var form_header = $('#form_header')
-
-function formChange(oldForm, newForm) {
-    var oldH = oldForm.offsetHeight;//oldForm.height();
-    var newH = newForm.offsetHeight;//newForm.height();
-    oldForm.fadeToggle(500, function() {
-        modal_form.animate({
-            height: newH
-        }, 500, function() {
-            newForm.fadeToggle(500);
-        });
-    });
+var avtgHeader = function() {
+  $('.avtg').css('display', 'none');
+  $('#avtg').css('display', 'block');
+  console.log('success');
 }
+function avtg(e){
+  if(e.target !== e.currentTarget){
+    var clickedItem = e.target.id;
+    var showAreaId='#'+clickedItem.match(/(.*?)_btn/)[1];
+    $('.avtg').css('display', 'none');
+    $('#avtg').css('display', 'none');
+    $(showAreaId).css('display', 'block');
+    console.log(showAreaId);
+  }
+  e.stopPropagation();
+}
+$('#avtg_btn').click(avtgHeader).hover(avtgHeader);
 
-document.getElementById('login_lost_btn').addEventListener("click", function(){
-  form_header.fadeOut(100,function(){
-    $(this).text("Forget Password").fadeIn(500);
-  });
-  formChange(login_form, forget_form);
-});
-document.getElementById('login_register_btn').addEventListener("click", function(){
-  form_header.fadeOut(100,function(){
-    $(this).text("Register").fadeIn(500);
-  });
-  formChange(login_form, register_form);
-});
-document.getElementById('register_sign_btn').addEventListener("click", function(){
-  form_header.fadeOut(100,function(){
-    $(this).text("Sign In").fadeIn(500);
-  });
-  formChange(register_form, login_form);
-});
-document.getElementById('register_lost_btn').addEventListener("click", function(){
-  form_header.fadeOut(100,function(){
-    $(this).text("Forget Password").fadeIn(500);
-  });
-  formChange(register_form, forget_form);
-});
-document.getElementById('forget_sign_btn').addEventListener("click", function(){
-  form_header.fadeOut(100,function(){
-    $(this).text("Sign In").fadeIn(500);
-  });
-  formChange(forget_form, login_form);
-});
-document.getElementById('forget_register_btn').addEventListener("click", function(){
-  form_header.fadeOut(100,function(){
-    $(this).text("Register").fadeIn(500);
-  });
-  formChange(forget_form, register_form);
-});
-
-document.getElementById('login_btn').addEventListener('click', function(){
-  login_form[0].style.display="block";
-  forget_form[0].style.display="none";
-  register_form[0].style.display="none";
-  form_header[0].innerHTML="Sign In";
+var theParent = document.querySelector('#eventForListener');//use querySelector to reduce code using
+/*add multiple eventListener for vertical nav area*/
+("mousemove click".split(" ")).forEach(function(evt){
+  theParent.addEventListener(evt,avtg,false);
 });
