db6bee90161ddb330a168c249f67507e08272145
xjiangiao <xjtc19910103@gmail.com>
2016-10-09 15:35:16 -0400

    bug fix

--- script/index.js
+++ script/index.js
@@ -1,48 +1,55 @@
 /*This is for intro element, which make nav-element of intro section will change via hover or click*/
 var avtgHeader = function() {
-  $('.avtg').css('display', 'none');
-  $('.intro').css('background-image','url(../website_for_ad/mypic/home.jpg)')
-  $('#avtg').css('display', 'block');
-//  console.log('success');//only for code test
-}
-function avtg(e){
-  if(e.target !== e.currentTarget){
-    var clickedItem = e.target.id;
-    var showAreaId='#'+clickedItem.match(/(.*?)_btn/)[1];
-    var picPath='url(../website_for_ad/mypic/'+clickedItem.match(/(.*?)_btn/)[1]+'.jpg)';
-    console.log(picPath);
     $('.avtg').css('display', 'none');
-    $('#avtg').css('display', 'none');
-    $(showAreaId).css('display', 'block');
-    $('.intro').css('background-image',picPath);
-    //    console.log(showAreaId);//only for code test
-  }
-  e.stopPropagation();
+    $('#avtg').css('display', 'block');
+    console.log('success');
 }
-$('#avtg_btn').click(avtgHeader).hover(avtgHeader);
+var tmp='#avtg';
+function avtg(e) {
+    if (e.target !== e.currentTarget) {
+        var clickedItem = e.target.id;
+        //    console.log(clickedItem);
+        if (clickedItem === '') {
+            e.stopPropagation();
+        } else {
+            var showAreaId = '#' + clickedItem.match(/(.*?)_btn/)[1];
+            //    console.log(showAreaId);
+            if(showAreaId!==tmp){
+            //   $('.avtg').css('display', 'none');
+               $(tmp).css('display', 'none');
+               $(showAreaId).css('display', 'block');
 
-var theParent = document.querySelector('#eventForListener');//use querySelector to reduce code using
+               tmp=showAreaId;
+             }
+        }
+    }
+    e.stopPropagation();
+}
+//$('#avtg_btn').click(avtgHeader).hover(avtgHeader);
+
+var theParent = document.querySelector('#eventForListener'); //use querySelector to reduce code using
 /*add multiple eventListener for vertical nav area*/
-("mousemove click".split(" ")).forEach(function(evt){
-  theParent.addEventListener(evt,avtg,false);
-});
+//("mouseover click".split(" ")).forEach(function(evt){
+theParent.addEventListener('mouseover', avtg, false);
+//});
+
 /*Add scroll effect for elelemt*/
 (function($) {
 
-  $.fn.visible = function(partial) {
+    $.fn.visible = function(partial) {
 
-      var $t            = $(this),
-          $w            = $(window),
-          viewTop       = $w.scrollTop(),
-          viewBottom    = viewTop + $w.height(),
-          _top          = $t.offset().top,
-          _bottom       = _top + $t.height(),
-          compareTop    = partial === true ? _bottom : _top,
-          compareBottom = partial === true ? _top : _bottom;
+        var $t = $(this),
+            $w = $(window),
+            viewTop = $w.scrollTop(),
+            viewBottom = viewTop + $w.height(),
+            _top = $t.offset().top,
+            _bottom = _top + $t.height(),
+            compareTop = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
 
-    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
 
-  };
+    };
 
 })(jQuery);
 
@@ -51,19 +58,19 @@ var win = $(window);
 var allMods = $(".module");
 
 allMods.each(function(i, el) {
-  var el = $(el);
-  if (el.visible(true)) {
-    el.addClass("already-visible");
-  }
+    var el = $(el);
+    if (el.visible(true)) {
+        el.addClass("already-visible");
+    }
 });
 
 win.scroll(function(event) {
 
-  allMods.each(function(i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-      el.addClass("come-in");
-    }
-  });
+    allMods.each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            el.addClass("come-in");
+        }
+    });
 
 });
